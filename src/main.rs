823a1928f9dd127fe290853fb7b40b1ce348106c
Javier Peratla <nottu1990@gmail.com>
2025-09-01 11:38:07 -0700

    use tokio and process multiple connections on spawned tasks

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-use std::{io::Write, net::TcpListener};
+use std::{net::SocketAddr, sync::Arc};
 
-fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379")?;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+    sync::Semaphore,
+};
 
-    for stream in listener.incoming() {
-        let mut stream = stream?;
-        stream.write_all(b"+PONG\r\n")?;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    let max_connections = Arc::new(Semaphore::new(10));
+    loop {
+        let (mut stream, addr) = listener.accept().await?;
+        let Ok(permit) = max_connections.clone().acquire_owned().await else {
+            stream
+                .write_all(b"Too many active connections, try later")
+                .await?;
+            continue;
+        };
+
+        tokio::spawn(async move {
+            if let Err(e) = process_connection(stream, addr).await {
+                eprint!("Error handling connection from {addr}: {e:?}");
+            }
+            drop(permit);
+        });
     }
-    Ok(())
+}
+
+async fn process_connection(mut stream: TcpStream, _addr: SocketAddr) -> anyhow::Result<()> {
+    let mut buf = [0u8; 1024];
+    loop {
+        let _bytes_read = stream.read(&mut buf).await?;
+        stream.write_all(b"+PONG\r\n").await?;
+    }
 }
