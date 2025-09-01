7c200e0c66aba68f0040386c8b5403108bdf2c3c
Javier Peratla <nottu1990@gmail.com>
2025-09-01 11:13:53 -0700

    use anyhow error and respond to stream with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let mut stream = stream?;
+        stream.write_all(b"+PONG\r\n")?;
     }
+    Ok(())
 }
